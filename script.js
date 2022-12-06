854ba904f0efd330cdc4f9758eb7064f9e2e1654
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-12-06 14:56:56 +0000

    Add getCell method to gameBoard

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const el = document.querySelector('#boardContainer');
 const cell = el.querySelectorAll('.cell');
 
 const gameBoard = (function () {
-  const _board = Array(9).fill('');
-//  const _board = ["x", "x", "o", "o", "x", "o", "x", "o", "o"];
+  //const _board = Array(9).fill('');
+  const _board = ["x", "x", "o", "o", "x", "o", "x", "o", "o"];
 
   const resetBoard = () => {
     for (let i = 0; i < _board.length; i++) {
@@ -12,14 +12,28 @@ const gameBoard = (function () {
     }
   };
 
+  const getCell = () => {
+    cell.forEach(cell => {
+      cell.addEventListener('click', () => {
+        return console.log(cell.id, cell.textContent);
+      })
+    });
+  }
+
   const render = () => {
     cell.forEach((cell) => {
       cell.textContent = _board[cell.id];
     });
   }
   return {
-    resetBoard, render,
+    resetBoard,
+    render,
+    getCell,
   };
 })();
+
+
+
+
 
 console.log('stop');
