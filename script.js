f465f462d4774e5671a8c7786834448498cd74a4
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-12-06 13:07:32 +0000

    Cache DOM elements

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+//cacheDOM
+const el = document.querySelector('#boardContainer');
+const cell = el.querySelectorAll('.cell');
+
 const gameBoard = (function () {
   const _board = Array(9).fill('');
+//  const _board = ["x", "x", "o", "o", "x", "o", "x", "o", "o"];
 
   const resetBoard = () => {
     for (let i = 0; i < _board.length; i++) {
