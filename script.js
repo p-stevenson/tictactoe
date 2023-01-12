5646734e23dff2ddc70f2ef67df0a7771d80c257
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2023-01-12 08:26:42 +0000

    Update script.js after eslint and prettier install

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-//cacheDOM
-const el = document.querySelector('#boardContainer');
-const cell = el.querySelectorAll('.cell');
+// eslint-disable-next-line no-unused-vars
+const tictactoe = (function () {
+  // cacheDOM
+  const el = document.querySelector('#mainContainer')
+  const cell = el.querySelectorAll('.cell')
+  const button = el.querySelectorAll('.button')
 
-const gameBoard = (function () {
-  //const _board = Array(9).fill('');
-  const _board = ["x", "x", "o", "o", "x", "o", "x", "o", "o"];
+  // bind events
+  button.forEach((button) => {
+    button.addEventListener('click', _getID, { once: true })
+  })
 
-  const getCell = () => {
-    function logCellData(cell) {
-      return cell.id;
+  cell.forEach((cell) => {
+    cell.addEventListener('click', _getID, { once: true })
+  })
+
+  function _getID () {
+    console.log(this.id)
+    return this.id
+  }
+
+  const gameBoard = function () {
+    // const _board = Array(9).fill('');
+    const _board = ['x', 'x', 'o', 'o', 'x', 'o', 'x', 'o', 'o']
+
+    const getCell = () => {}
+
+    const render = () => {
+      cell.forEach((cell) => {
+        cell.textContent = _board[cell.id]
+      })
     }
-    cell.forEach(cell => {
-      cell.addEventListener('click', logCellData, {once :true})
-    });
-  };
-
-  const render = () => {
-    cell.forEach((cell) => {
-      cell.textContent = _board[cell.id];
-    });
-  };
-  const resetBoard = () => {
-    for (let i = 0; i < _board.length; i++) {
-      _board[i] = '';
+    const resetBoard = () => {
+      for (let i = 0; i < _board.length; i++) {
+        _board[i] = ''
+      }
     }
-  };
-  return {
-    getCell,
-    render,
-    resetBoard,
-  };
-})();
+    return {
+      getCell,
+      render,
+      resetBoard
+    }
+  }
+  return { gameBoard }
+})()
 
-console.log('stop');
+console.log('stop')
